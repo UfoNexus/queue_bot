241a2110888a0db6ff038a75845d779582c9a2ed
UfoNexus <evans.d@yandex.ru>
2021-09-22 18:59:22 +0300

    fixed numeration

--- bot.py
+++ bot.py
@@ -16,41 +16,40 @@ def on_start(update, context):
 
 
 current_queue = {}
-queue_username_list = []
+queue_id_list = []
 queue_name_list = []
 
 
 def get_in_queue(update, context):
     user = update.message.from_user
-    active_username = user['username']
-    active_truename = f'{user["first_name"]} {user["last_name"]}'
-    queue_username_list = [(current_queue[i])[0] for i in current_queue]
-    queue_name_list = [(current_queue[i])[1] for i in current_queue]
-    if active_username not in queue_username_list:
+    active_id = user['id']
+    if active_id not in queue_id_list:
+        if user['last_name'] is None:
+            active_truename = user["first_name"]
+        else:
+            active_truename = f'{user["first_name"]} {user["last_name"]}'
         queue_number = len(current_queue) + 1
-        current_queue[queue_number] = active_truename
-        queue_username_list.append((current_queue[queue_number])[0])
-        queue_name_list.append(current_queue[1])
+        current_queue[queue_number] = active_id, active_truename
+        queue_id_list.append(current_queue[queue_number][0])
+        queue_name_list.append(current_queue[queue_number][1])
         queue_name_list_numbered = []
         for i, name in enumerate(queue_name_list):
             queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-        update.message.reply_text(f'{active_truename} {QUEUE_SUCCESS}',
-                                  quote=True)
-        update.message.reply_text(queue_name_list_numbered[0])
-        update.message.reply_text(queue_name_list)
-        update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
-    elif active_username in queue_username_list:
+        update.message.reply_text(f'{active_truename} {QUEUE_SUCCESS}')
+        update.message.reply_text(QUEUE_CURRENT +
+                                  '\n'.join(queue_name_list_numbered))
+    else:
         queue_name_list_numbered = []
         for i, name in enumerate(queue_name_list):
             queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-        update.message.reply_text(QUEUE_ERROR,
-                                  quote=True)
-        update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
+        update.message.reply_text(QUEUE_ERROR, quote=True)
+        update.message.reply_text(QUEUE_CURRENT +
+                                  '\n'.join(queue_name_list_numbered))
 
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("start", on_start))
-dispatcher.add_handler(CommandHandler("get_in_queue", get_in_queue))
+dispatcher.add_handler(CommandHandler("get_in", get_in_queue))
 
 updater.start_polling()
 updater.idle()
