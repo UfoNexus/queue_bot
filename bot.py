02d1da050ba384b756976af0a7b6544240ff68fd
UfoNexus <evans.d@yandex.ru>
2021-09-22 17:54:44 +0300

    merged master

--- bot.py
+++ bot.py
@@ -26,26 +26,26 @@ def get_in_queue(update, context):
     active_truename = f'{user["first_name"]} {user["last_name"]}'
     queue_username_list = [(current_queue[i])[0] for i in current_queue]
     queue_name_list = [(current_queue[i])[1] for i in current_queue]
-    #if active_username not in queue_username_list:
-    #    queue_number = len(current_queue) + 1
-    #    current_queue[queue_number] = active_truename
-    #    queue_username_list.append((current_queue[queue_number])[0])
-    #    queue_name_list.append(current_queue[1])
-    #    queue_name_list_numbered = []
-    #    for i, name in enumerate(queue_name_list):
-    #        queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-    #    update.message.reply_text(f'{active_truename} {QUEUE_SUCCESS}',
-    #                              quote=True)
-    #    update.message.reply_text(queue_name_list_numbered[0])
-    #    update.message.reply_text(queue_name_list)
-    #    update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
-    #elif active_username in queue_username_list:
-    #    queue_name_list_numbered = []
-    #    for i, name in enumerate(queue_name_list):
-    #        queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-    #    update.message.reply_text(QUEUE_ERROR,
-    #                              quote=True)
-    #    update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
+    if active_username not in queue_username_list:
+        queue_number = len(current_queue) + 1
+        current_queue[queue_number] = active_truename
+        queue_username_list.append((current_queue[queue_number])[0])
+        queue_name_list.append(current_queue[1])
+        queue_name_list_numbered = []
+        for i, name in enumerate(queue_name_list):
+            queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
+        update.message.reply_text(f'{active_truename} {QUEUE_SUCCESS}',
+                                  quote=True)
+        update.message.reply_text(queue_name_list_numbered[0])
+        update.message.reply_text(queue_name_list)
+        update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
+    elif active_username in queue_username_list:
+        queue_name_list_numbered = []
+        for i, name in enumerate(queue_name_list):
+            queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
+        update.message.reply_text(QUEUE_ERROR,
+                                  quote=True)
+        update.message.reply_text(QUEUE_CURRENT + "\n".join(queue_name_list_numbered))
 
 
 dispatcher = updater.dispatcher
