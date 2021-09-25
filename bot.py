21b0487ebbbe6db0ac37251a7b894bf9cee87db2
UfoNexus <evans.d@yandex.ru>
2021-09-25 23:34:28 +0300

    added buttons and optimised code

--- bot.py
+++ bot.py
@@ -1,37 +1,75 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Bot
 
 
 print('Бот запущен. Нажмите Ctrl+C для завершения')
 
-GREETING = 'Привет, я бот очередей'
+GREETING = 'В очереди никто не стоит'
 QUEUE_SUCCESS = 'встал в очередь'
 QUEUE_ALREADY_IN = 'Ты уже стоишь в очереди'
 QUEUE_CURRENT = 'Стоят в очереди:\n'
 QUEUE_COMPLETE = 'В очереди больше никого нет'
 QUEUE_NEXT = ', к тебе взывают'
 ADMIN_CONTROL = 'Взывать к людям из очереди может только админ группы'
+BUTTON_CALL_NEXT = 'Возвать к следующему'
+BUTTON_CALL_CURRENT = 'Разбудить'
+WARNING = ('Кто-то все еще стоит в очереди.\n'
+           'Очистите ее командой /clear.')
 
 token = '2011946261:AAFClQ54uJ9UvKiwBv4Fipcn47cEwxv7szQ'
 updater = Updater(token, use_context=True)
 
 
+chat_id = 0
 current_queue = {}
 queue_id_list = []
 queue_name_list = []
 queue_counter = 0
+queue_message_id = 0
 
 
 def get_admin_ids(bot, chat_id):
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
 
 
+def buttons_setup():
+    button_list = [
+        [
+            InlineKeyboardButton(BUTTON_CALL_NEXT, callback_data='/call'),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(button_list)
+    return reply_markup
+
+
 def on_start(update, context):
-    update.message.reply_text(GREETING, quote=False)
+    global chat_id
+    chat_id = update.effective_chat.id
+    message_id = update.effective_message.message_id
+    if len(current_queue) == 0:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=GREETING,
+            reply_markup=buttons_setup()
+        )
+        global queue_message_id
+        queue_message_id = message_id + 1
+        context.bot.pinChatMessage(
+            chat_id=chat_id,
+            message_id=queue_message_id
+        )
+    else:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=WARNING
+        )
 
 
 def get_in_queue(update, context):
+    global chat_id
     user = update.message.from_user
     active_id = user['id']
+    global queue_message_id
     if active_id not in queue_id_list:
         if user['last_name'] is None:
             active_truename = user["first_name"]
@@ -44,41 +82,93 @@ def get_in_queue(update, context):
         queue_name_list_numbered = []
         for i, name in enumerate(queue_name_list):
             queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-        update.message.reply_text(f'{active_truename} {QUEUE_SUCCESS}')
-        update.message.reply_text(QUEUE_CURRENT +
-                                  '\n'.join(queue_name_list_numbered),
-                                  quote=False)
+        update.message.reply_text(
+            f'{active_truename} {QUEUE_SUCCESS}'
+        )
+        context.bot.edit_message_text(
+            QUEUE_CURRENT + '\n'.join(queue_name_list_numbered),
+            chat_id=chat_id,
+            message_id=queue_message_id,
+            reply_markup=buttons_setup()
+        )
     else:
         queue_name_list_numbered = []
         for i, name in enumerate(queue_name_list):
             queue_name_list_numbered.append(f'{i+1}. {queue_name_list[i]}')
-        update.message.reply_text(QUEUE_ALREADY_IN)
-        update.message.reply_text(QUEUE_CURRENT +
-                                  '\n'.join(queue_name_list_numbered),
-                                  quote=False)
+        update.message.reply_text(
+            QUEUE_ALREADY_IN
+        )
 
 
 def call_next(update, context):
-    if update.effective_user.id in get_admin_ids(context.bot,
-                                                 update.message.chat_id):
+    global chat_id
+    if update.effective_user.id in get_admin_ids(
+        context.bot,
+        update.message.chat_id
+    ):
         global queue_counter
         if queue_counter < len(current_queue):
             queue_counter += 1
             next_user = (current_queue[queue_counter][0],
                          current_queue[queue_counter][1])
             next_userlink = f'(tg://user?id={str(next_user[0])})'
-            next_user_mention = f'[{next_user[1]}]{next_userlink}{QUEUE_NEXT}'
-            update.message.reply_text(next_user_mention, parse_mode="Markdown")
+            next_user_mention = f'[{next_user[1]}]{next_userlink}'
+            context.bot.send_message(
+                chat_id=chat_id,
+                text=f'{next_user_mention}{QUEUE_NEXT}',
+                parse_mode="Markdown"
+            )
         else:
-            update.message.reply_text(QUEUE_COMPLETE, quote=False)
+            context.bot.send_message(
+                chat_id=chat_id,
+                text=QUEUE_COMPLETE
+            )
     else:
-        update.message.reply_text(ADMIN_CONTROL, quote=False)
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=ADMIN_CONTROL
+        )
+
+
+def button(update, context):
+    global chat_id
+    if update.effective_user.id in get_admin_ids(
+        context.bot,
+        update.callback_query.message.chat_id
+    ):
+        bot = Bot(token=token)
+        query = update.callback_query
+        if query.data == '/call':
+            global queue_counter
+            if queue_counter < len(current_queue):
+                queue_counter += 1
+                next_user = (current_queue[queue_counter][0],
+                             current_queue[queue_counter][1])
+                next_userlink = f'(tg://user?id={str(next_user[0])})'
+                next_user_mention = (
+                    f'[{next_user[1]}]{next_userlink}{QUEUE_NEXT}'
+                )
+                bot.sendMessage(
+                    chat_id=chat_id,
+                    text=next_user_mention,
+                    parse_mode="Markdown")
+            else:
+                bot.sendMessage(
+                    chat_id=chat_id,
+                    text=QUEUE_COMPLETE
+                )
+    else:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text=ADMIN_CONTROL
+        )
 
 
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler("start", on_start))
 dispatcher.add_handler(CommandHandler("get_in", get_in_queue))
 dispatcher.add_handler(CommandHandler("call", call_next))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
 updater.start_polling()
 updater.idle()
--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-test = {
-    1: 'test1',
-    2: 'test2',
-    3: 'test3'
-}
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-print(len(test))
+
+print('Бот запущен. Нажмите Ctrl+C для завершения')
+
+token = '2011946261:AAFClQ54uJ9UvKiwBv4Fipcn47cEwxv7szQ'
+updater = Updater(token, use_context=True)
+
+
+def start(update, context):
+    
+
+
+def button(update, context):
+    chat = update.effective_chat
+    query = update.callback_query
+    query.answer()
+    context.bot.send_message(chat_id=chat.id, text=f"Selected option: {query.data}")
+
+
+dispatcher = updater.dispatcher
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+updater.start_polling()
+updater.idle()
